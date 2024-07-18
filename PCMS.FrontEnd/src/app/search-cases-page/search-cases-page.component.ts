90220819fbc2a8121593fadf24229a28efd49e39
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-18 09:25:09 +0100

    added basic search case number input

--- PCMS.FrontEnd/src/app/search-cases-page/search-cases-page.component.ts
+++ PCMS.FrontEnd/src/app/search-cases-page/search-cases-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-cases-page',
   templateUrl: './search-cases-page.component.html',
-  styleUrl: './search-cases-page.component.css'
+  styleUrl: './search-cases-page.component.css',
 })
 export class SearchCasesPageComponent {
+  
+  SearchCaseNumberInput = new FormControl('', [
+    Validators.minLength(8),
+    Validators.maxLength(30),
+    Validators.pattern(/^CA/),
+  ]);
 
+  /* Form group for optional fields  */
 }
