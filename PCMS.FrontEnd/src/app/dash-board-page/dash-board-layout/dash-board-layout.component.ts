e365470439391cb7c9d4e1e6de4991ca0da28cd1
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-21 12:55:48 +0100

    close sidenav

--- PCMS.FrontEnd/src/app/dash-board-page/dash-board-layout/dash-board-layout.component.ts
+++ PCMS.FrontEnd/src/app/dash-board-page/dash-board-layout/dash-board-layout.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dash-board-layout',
   templateUrl: './dash-board-layout.component.html',
-  styleUrl: './dash-board-layout.component.css'
+  styleUrl: './dash-board-layout.component.css',
 })
 export class DashBoardLayoutComponent {
-
+  isSideNavOpen = true;
 }
