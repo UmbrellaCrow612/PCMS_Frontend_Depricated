003cf4f292cd1fb8b96fc0b92aca583654f7b99f
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-22 10:48:41 +0100

    created a skeleton application db context

--- PCMS.API/Models/People/Suspect.cs
+++ PCMS.API/Models/People/Suspect.cs
@@ -3,7 +3,13 @@
     internal class Suspect : Person
     {
         public required string Alias { get; set; }
+
         public required string Description { get; set; }
+
         public ICollection<Case> Cases { get; set; } = [];
+
+        public required string PersonId { get; set; }
+
+        public required Person Person { get; set; }
     }
 }
