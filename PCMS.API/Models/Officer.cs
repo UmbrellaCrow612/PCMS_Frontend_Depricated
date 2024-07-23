6f4196c6a2d0613512a9af6d5204787773eec6c1
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-23 08:57:53 +0100

    data annotations

--- PCMS.API/Models/Officer.cs
+++ PCMS.API/Models/Officer.cs
@@ -1,18 +1,17 @@
-﻿namespace PCMS.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Models
 {
     internal class Officer : Person
     {
+        [Required]
         public required string BadgeNumber { get; set; }
 
+        [Required]
         public required string Username { get; set; }
 
+        [Required]
         public required string PasswordHash { get; set; }
-
-        public required string DepartmentId { get; set; }
-
-        public required Department Department { get; set; }
-
-        public ICollection<Case> AssignedCases { get; set; } = [];
 
     }
 }
--- PCMS.API/Models/Person.cs
+++ PCMS.API/Models/Person.cs
@@ -1,35 +1,49 @@
-﻿namespace PCMS.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Models
 {
     internal class Person
     {
+        [Required]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
+        [Required]
         public required string FirstName { get; set; }
 
         public string? MiddleName { get; set; }
 
+        [Required]
         public required string LastName { get; set; }
 
+        [Required]
         public required DateTime DateOfBirth { get; set; }
 
         public string? Email { get; set; }
 
+        [Required]
         public required string PhoneNumber { get; set; }
 
         public string? ProfileImgUrl { get; set; }
 
+        [Required]
         public required string Address { get; set; }
 
+        [Required]
         public required string Gender { get; set; }
 
+        [Required]
         public required string BirthPlace { get; set; }
 
+        [Required]
         public required double Height { get; set; }
 
+        [Required]
         public required string Race { get; set; }
 
+        [Required]
         public required string Ethnicity { get; set; }
 
+        [Required]
         public required string Nationality { get; set; }
 
         public string? PhysicalDescription { get; set; }
@@ -39,6 +53,12 @@
         public string? DriversLicenseNumber { get; set; }
 
         public string? NationalInsuranceNumber { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
 
     }
 }
