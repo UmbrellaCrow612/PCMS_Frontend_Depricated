b3a51323bc95489c1477f7a94145ff30000a8919
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-23 14:04:34 +0100

    no more iherits

--- API/Models/Officer.cs
+++ API/Models/Officer.cs
@@ -2,6 +2,48 @@
 {
     public class Officer
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public required string FirstName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required DateTime DateOfBirth { get; set; }
+
+        public string? Email { get; set; }
+
+        public required string PhoneNumber { get; set; }
+
+        public required string ProfileImgUrl { get; set; }
+
+        public required string Address { get; set; }
+
+        public required string Gender { get; set; }
+
+        public required string BirthPlace { get; set; }
+
+        public required double Height { get; set; }
+
+        public required string Race { get; set; }
+
+        public required string Ethnicity { get; set; }
+
+        public required string Nationality { get; set; }
+
+        public string? PhysicalDescription { get; set; }
+
+        public string? GeneralDescription { get; set; }
+
+        public string? DriversLicenseNumber { get; set; }
+
+        public string? NationalInsuranceNumber { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
+
         public required string BadgeNumber { get; set; }
 
         public required string Username { get; set; }
