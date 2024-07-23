d5d8fa1920afb9ec72db76a513d0e18cb3b3b499
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-23 09:27:57 +0100

    more data anno

--- PCMS.API/Models/Report.cs
+++ PCMS.API/Models/Report.cs
@@ -1,7 +1,26 @@
-﻿namespace PCMS.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Models
 {
     internal class Report
     {
+        [Required]
         public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public required string CaseNUm { get; set; }
+
+        [Required]
+        public required DateTime DateCreated { get; set; }
+
+        [Required]
+        public required string Officer { get; set; }
+
+        [Required]
+        public required string Inci { get; set; }
+
+        [Required]
+        public required string Details { get; set; }
+
     }
 }
--- PCMS.API/Models/Status.cs
+++ PCMS.API/Models/Status.cs
@@ -1,11 +1,16 @@
-﻿namespace PCMS.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Models
 {
     internal class Status
     {
+        [Required]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
+        [Required]
         public required string Name { get; set; }
 
+        [Required]
         public required string Description { get; set; }
     }
 }
