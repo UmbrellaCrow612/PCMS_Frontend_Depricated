79933626ef484685be02593824fcbeec6f1b2650
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-24 16:43:01 +0100

    toggle side nav pref

--- PCMS.FrontEnd/src/app/views/dashboard/dashboard-page.component.ts
+++ PCMS.FrontEnd/src/app/views/dashboard/dashboard-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrl: './dashboard-page.component.css'
+  styleUrl: './dashboard-page.component.css',
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit {
+  isSideNavOpen = true;
 
+  ngOnInit(): void {
+    this.setSideNavPrefOption();
+  }
+
+  setSideNavPrefOption() {
+    const pref = localStorage.getItem('sideNavOpenPref');
+
+    if (pref !== null) {
+      this.isSideNavOpen = pref === 'true';
+    } else {
+      localStorage.setItem('sideNavOpenPref', this.isSideNavOpen.toString());
+    }
+  }
+
+  toggleSideNav() {
+    this.isSideNavOpen = !this.isSideNavOpen;
+    localStorage.setItem('sideNavOpenPref', this.isSideNavOpen.toString());
+  }
 }
