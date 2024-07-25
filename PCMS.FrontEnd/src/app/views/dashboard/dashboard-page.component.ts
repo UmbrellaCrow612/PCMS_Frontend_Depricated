727c3c73c3c71a76420e3de1eacaeb739d30bbff
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-25 10:38:15 +0100

    fully responsive side nav

--- PCMS.FrontEnd/src/app/views/dashboard/dashboard-page.component.ts
+++ PCMS.FrontEnd/src/app/views/dashboard/dashboard-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MatDrawerMode } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css',
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
   isSideNavOpen = true;
+  destroyed = new Subject<void>();
+  drawerMode: MatDrawerMode = 'side';
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([Breakpoints.Medium, Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe((result) => {
+        if (result.matches) {
+          this.drawerMode = 'over';
+        } else {
+          this.drawerMode = 'side';
+        }
+      });
+  }
 
   ngOnInit(): void {
     this.setSideNavPrefOption();
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
   setSideNavPrefOption() {
