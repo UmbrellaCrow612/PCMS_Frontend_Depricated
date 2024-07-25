fd39060587b06da63866445b04b6246f3a408613
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-25 14:13:34 +0100

    addede dashboard homepage loading state

--- PCMS.FrontEnd/src/app/views/dashboard/home/dashboard-home-page.component.ts
+++ PCMS.FrontEnd/src/app/views/dashboard/home/dashboard-home-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-home-page',
   templateUrl: './dashboard-home-page.component.html',
-  styleUrl: './dashboard-home-page.component.css'
+  styleUrl: './dashboard-home-page.component.css',
 })
-export class DashboardHomePageComponent {
+export class DashboardHomePageComponent implements OnInit {
+  isLoading = true;
 
+  loadDashboardHomePageData() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+    this.loadDashboardHomePageData();
+  }
 }
