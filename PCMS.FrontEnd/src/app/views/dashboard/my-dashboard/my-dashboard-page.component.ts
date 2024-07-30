693a6b301a27ffda8190b034b9d1507e19477a4b
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-30 13:41:22 +0100

    added my case status pie chart

--- PCMS.FrontEnd/src/app/views/dashboard/my-dashboard/my-dashboard-page.component.ts
+++ PCMS.FrontEnd/src/app/views/dashboard/my-dashboard/my-dashboard-page.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-my-dashboard-page',
   templateUrl: './my-dashboard-page.component.html',
-  styleUrl: './my-dashboard-page.component.css'
+  styleUrl: './my-dashboard-page.component.css',
 })
 export class MyDashboardPageComponent {
+  CaseStatusView: any = undefined;
+  CaseStatusColorScheme: Color = {
+    name: 'status',
+    selectable: true,
+    group: ScaleType.Linear,
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
+  CaseStatusResults = [
+    {
+      name: 'Open',
+      value: 12,
+    },
+    {
+      name: 'Closed',
+      value: 21,
+    },
+    {
+      name: 'Pending',
+      value: 22,
+    },
+    {
+      name: 'UI',
+      value: 11,
+    },
+  ];
+  CaseStatusLegendTitle = 'Case';
 
+  onCaseStatusResize(width: number, height: number) {
+    this.CaseStatusView = [width, height];
+  }
 }
