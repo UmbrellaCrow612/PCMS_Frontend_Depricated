987e7d91415b380769f92d3886a12994a3ce379d
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-31 11:57:49 +0100

    added two charts

--- PCMS.FrontEnd/src/app/views/dashboard/incident-analysis/incident-analysis-page.component.ts
+++ PCMS.FrontEnd/src/app/views/dashboard/incident-analysis/incident-analysis-page.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-incident-analysis-page',
   templateUrl: './incident-analysis-page.component.html',
-  styleUrl: './incident-analysis-page.component.css'
+  styleUrl: './incident-analysis-page.component.css',
 })
 export class IncidentAnalysisPageComponent {
+  CrimeIncidentCountResult = [
+    {
+      name: 'Assault',
+      value: 340,
+    },
+    {
+      name: 'Theft',
+      value: 200,
+    },
+    {
+      name: 'Robbery',
+      value: 100,
+    },
+  ];
+  CrimeIncidentCountView: any = undefined;
 
+  onCrimeIncidentCountResize(width: number, height: number) {
+    this.CrimeIncidentCountView = [width, height];
+  }
+
+  CrimeIncidentBreakdownResult = [
+    {
+      name: 'Assault',
+      value: 340,
+    },
+    {
+      name: 'Theft',
+      value: 200,
+    },
+    {
+      name: 'Robbery',
+      value: 100,
+    },
+  ];
+  CrimeIncidentBreakdownView: any = undefined;
+
+  onCrimeIncidentBreakdownResize(width: number, height: number) {
+    this.CrimeIncidentBreakdownView = [width, height];
+  }
 }
