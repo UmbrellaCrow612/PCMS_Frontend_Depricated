2f4bc2fa3ad50ae35c0037f150a3cb1486a294c2
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-04 19:50:43 +0100

    ngx chart dark mode styles

--- PCMS.FrontEnd/src/app/theme/theme-service.service.ts
+++ PCMS.FrontEnd/src/app/theme/theme-service.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class ThemeService {
-  private darkMode = new BehaviorSubject<boolean>(true);
+  private darkMode = new BehaviorSubject<boolean>(false);
   isDarkMode$ = this.darkMode.asObservable();
 
   toggleDarkMode() {
