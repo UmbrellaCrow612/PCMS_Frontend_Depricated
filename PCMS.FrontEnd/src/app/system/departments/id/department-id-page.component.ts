0851f4809ebd214faaaa723a23f63be1b716ead8
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-06 10:40:38 +0100

    added basic forms to dep id page

--- PCMS.FrontEnd/src/app/system/departments/id/department-id-page.component.ts
+++ PCMS.FrontEnd/src/app/system/departments/id/department-id-page.component.ts
@@ -1,27 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-department-id-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './department-id-page.component.html',
   styleUrl: './department-id-page.component.scss',
 })
 export class DepartmentIdPageComponent implements OnInit {
   isPageLoading: boolean = true;
   // Have states for different views like error , not found etc
-  departmentId:any; // set the dep name to the title 
+  departmentId: any; // set the dep name to the title
   department: any;
+
+  editedDepartmentForm = new FormGroup({
+    name: new FormControl('123456', [
+      Validators.minLength(5),
+      Validators.maxLength(55),
+      Validators.required,
+    ]),
+    description: new FormControl('123456', [
+      Validators.minLength(5),
+      Validators.maxLength(150),
+      Validators.required,
+    ]),
+    shortCode: new FormControl('1234', [
+      Validators.minLength(3),
+      Validators.maxLength(15),
+      Validators.required,
+    ]),
+  });
 
   constructor(private route: ActivatedRoute, private titleService: Title) {}
 
   ngOnInit(): void {
     this.isPageLoading = false;
     this.route.paramMap.subscribe((params) => {
-      this.departmentId  = params.get('id');
+      this.departmentId = params.get('id');
     });
-    this.titleService.setTitle(this.departmentId)
+    this.titleService.setTitle(this.departmentId);
   }
+
+  onSubmit() {}
 }
