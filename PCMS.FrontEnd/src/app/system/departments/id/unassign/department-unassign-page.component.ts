d06fa5e3e58f2e8ca1cfddfac5a53daf0210c24a
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-06 15:59:06 +0100

    Unassign officer from dep

--- PCMS.FrontEnd/src/app/system/departments/id/unassign/department-unassign-page.component.ts
+++ PCMS.FrontEnd/src/app/system/departments/id/unassign/department-unassign-page.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { ColDef } from 'ag-grid-community';
+import { GridComponent } from '../../../../grid/grid.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-department-unassign-page',
   standalone: true,
-  imports: [],
+  imports: [GridComponent, MatButtonModule],
   templateUrl: './department-unassign-page.component.html',
   styleUrl: './department-unassign-page.component.scss',
 })
 export class DepartmentUnassignPageComponent implements OnInit {
   departmentId: any;
+
+  columnDefs: ColDef[] = [
+    { headerCheckboxSelection: true, checkboxSelection: true },
+    { headerName: 'ID', field: 'id' },
+    { headerName: 'Name', field: 'name' },
+    { headerName: 'Rank', field: 'rank' },
+  ];
+
+  rowData = [
+    { id: 1, name: 'Officer John Doe', rank: 'Sergeant' },
+    { id: 2, name: 'Officer Jane Smith', rank: 'Lieutenant' },
+    { id: 3, name: 'Officer Emily Davis', rank: 'Officer' },
+  ];
+
+  selectedOfficers: any[] = [];
+
+  handleSelectionChanged(selectedOfficers: any[]): void {
+    this.selectedOfficers = selectedOfficers;
+    console.log('Selected Officers:', this.selectedOfficers);
+  }
 
   constructor(private route: ActivatedRoute, private titleService: Title) {}
 
@@ -18,6 +41,8 @@ export class DepartmentUnassignPageComponent implements OnInit {
     this.route.paramMap.subscribe((params) => {
       this.departmentId = params.get('id');
     });
-    this.titleService.setTitle("PCMS - Unassign Officers for " +this.departmentId);
+    this.titleService.setTitle(
+      'PCMS - Unassign Officers for ' + this.departmentId
+    );
   }
 }
