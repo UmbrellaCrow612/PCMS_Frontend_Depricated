f65c2434f39c35a73aa13d55a60d60eaead782ae
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-07 10:21:17 +0100

    added all the form behavior fields

--- PCMS.FrontEnd/src/app/system/officers/create/create-officer-page.component.ts
+++ PCMS.FrontEnd/src/app/system/officers/create/create-officer-page.component.ts
@@ -1,12 +1,169 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-officer-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './create-officer-page.component.html',
-  styleUrl: './create-officer-page.component.scss'
+  styleUrl: './create-officer-page.component.scss',
 })
 export class CreateOfficerPageComponent {
+  officerForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.minLength(1),
+      Validators.maxLength(50),
+      Validators.required,
+    ]),
+    middleName: new FormControl('', [
+      Validators.minLength(1),
+      Validators.maxLength(50),
+    ]),
+    lastName: new FormControl('', [
+      Validators.minLength(1),
+      Validators.maxLength(50),
+      Validators.required,
+    ]),
+    dateOfBirth: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', [Validators.required]),
+    profileImgUrl: new FormControl('', [Validators.required]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(60),
+    ]),
+    gender: new FormControl('', [Validators.required]),
+    birthPlace: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(50),
+    ]),
+    height: new FormControl('', [Validators.required]),
+    race: new FormControl('', [Validators.required]),
+    ethnicity: new FormControl('', [Validators.required]),
+    nationality: new FormControl('', [Validators.required]),
+    physicalDescription: new FormControl(''),
+    generalDescription: new FormControl(''),
+    driversLicenseNumber: new FormControl('', [
+      Validators.required,
+      this.ukDriversLicenseValidator(),
+    ]),
+    nationalInsuranceNumber: new FormControl('', [
+      Validators.required,
+      this.ninoValidator(),
+    ]),
+    badgeNumber: new FormControl('', Validators.required),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.minLength(50),
+    ]),
+    accessLevel: new FormControl('', Validators.required),
+    departmentId: new FormControl('', Validators.required),
+  });
 
+  ninoValidator(): (control: AbstractControl) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value: string = control.value;
+
+      // Remove any spaces from the input
+      const cleanedValue = value.replace(/\s/g, '');
+
+      // Regular expression to match the basic NINO format
+      const ninoPattern = /^[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z][0-9]{6}[A-D]$/;
+
+      if (!ninoPattern.test(cleanedValue)) {
+        return { invalidNino: true };
+      }
+
+      // Check for invalid prefixes
+      const invalidPrefixes = [
+        'BG',
+        'GB',
+        'KN',
+        'NK',
+        'NT',
+        'TN',
+        'ZZ',
+        'OO',
+        'FY',
+        'NC',
+        'PP',
+        'PZ',
+      ];
+      const prefix = cleanedValue.substring(0, 2);
+
+      if (invalidPrefixes.includes(prefix)) {
+        return { invalidNinoPrefix: true };
+      }
+
+      return null;
+    };
+  }
+
+  ukDriversLicenseValidator(): (
+    control: AbstractControl
+  ) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value: string = control.value;
+
+      // Remove any spaces from the input
+      const cleanedValue = value.replace(/\s/g, '');
+
+      // Regular expression to match the UK driver's license format
+      const dlPattern = /^[A-Z]{5}[0-9]{6}[A-Z]{2}[0-9]{2}$/;
+
+      if (!dlPattern.test(cleanedValue)) {
+        return { invalidDriversLicense: true };
+      }
+
+      // Check if the date of birth encoded in the license is valid
+      const dobDigits = cleanedValue.substring(5, 11);
+      const decade = parseInt(dobDigits.substring(0, 2));
+      let year = decade < 50 ? 2000 + decade : 1900 + decade;
+      const month = parseInt(dobDigits.substring(2, 4));
+      const day = parseInt(dobDigits.substring(4, 6));
+
+      // Adjust for the century addition in months for people born before 2000
+      const adjustedMonth = month > 20 && year < 2000 ? month - 20 : month;
+      const adjustedYear = month > 20 && year < 2000 ? year + 100 : year;
+
+      const dob = new Date(adjustedYear, adjustedMonth - 1, day);
+      if (
+        isNaN(dob.getTime()) ||
+        dob > new Date() ||
+        dob < new Date(1900, 0, 1)
+      ) {
+        return { invalidDriversLicenseDate: true };
+      }
+
+      return null;
+    };
+  }
+
+  clearForm() {
+    this.officerForm.reset();
+  }
+
+  onSubmit() {
+    if (this.officerForm.valid) {
+      console.log(this.officerForm.value);
+    }
+  }
 }
