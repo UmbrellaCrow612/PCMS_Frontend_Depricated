35db35ee66995baa8d690ab2e09e21dd5869a910
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-12 10:39:56 +0100

    local_ID

--- PCMS.FrontEnd/src/main.ts
+++ PCMS.FrontEnd/src/main.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { errorHandlerFactory } from './app/errors/error-handler.factory';
-import { ErrorHandler, importProvidersFrom } from '@angular/core';
+import { ErrorHandler, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { GlobalErrorHandler } from './app/errors/global-error-handler';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -20,5 +20,6 @@ bootstrapApplication(AppComponent, {
       useFactory: errorHandlerFactory,
       deps: [GlobalErrorHandler],
     },
+    { provide: LOCALE_ID, useValue: 'en-US' },
   ],
 }).catch((err) => console.error(err));
