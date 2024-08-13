85399f09eb1b5da793ed7e0e58f37bd637e0d249
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-13 11:24:15 +0100

    officer id basic edit form done

--- PCMS.FrontEnd/src/app/system/officers/id/officer-id-page.component.ts
+++ PCMS.FrontEnd/src/app/system/officers/id/officer-id-page.component.ts
@@ -1,18 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ninoValidator, noLettersValidator } from '../../../validators';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { map, Observable, startWith } from 'rxjs';
+import { AccessLevel } from '../create/test-data';
+
+interface Department {
+  name: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-officer-id-page',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    CommonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+  ],
   templateUrl: './officer-id-page.component.html',
   styleUrl: './officer-id-page.component.scss',
 })
-export class OfficerIdPageComponent implements OnInit {
+export class OfficerIdPageComponent implements OnInit, OnDestroy {
   OfficerId: any = '';
   constructor(private route: ActivatedRoute, private titleService: Title) {}
 
@@ -20,29 +51,57 @@ export class OfficerIdPageComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.OfficerId = id;
     this.titleService.setTitle('PCMS - Officer details for ' + this.OfficerId);
+    this.filteredDepartments = this.basicOfficerForm
+      ?.get('departmentId')
+      ?.valueChanges.pipe(
+        startWith(''),
+        map((value) => this._departmentFilter(value || ''))
+      );
   }
 
-  typicallyEditableOfficerForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phoneNumber: new FormControl('', [
+  selectedImageUrl: string | null = null;
+  departments = [
+    {
+      name: 'dep one',
+      id: 'dep one',
+    },
+    {
+      name: 'dep two',
+      id: 'dep two',
+    },
+  ];
+  filteredDepartments: Observable<Department[]> | undefined;
+  accessLevels = Object.values(AccessLevel).filter(
+    (value) => typeof value === 'number'
+  );
+
+  basicOfficerForm = new FormGroup({
+    email: new FormControl('some@gmail.com', [
+      Validators.required,
+      Validators.email,
+    ]),
+    phoneNumber: new FormControl('07458734', [
       Validators.maxLength(15),
       Validators.required,
       noLettersValidator(),
     ]),
     address: new FormGroup({
-      houseNumber: new FormControl('', [Validators.required]),
-      streetName: new FormControl('', [Validators.required]),
-      town: new FormControl('', [Validators.required]),
-      postcode: new FormControl('', [Validators.required]),
+      houseNumber: new FormControl('33', [Validators.required]),
+      streetName: new FormControl('lop', [Validators.required]),
+      town: new FormControl('sheep', [Validators.required]),
+      postcode: new FormControl('S3', [Validators.required]),
     }),
     physicalDescription: new FormControl('', [Validators.maxLength(200)]),
     generalDescription: new FormControl('', [Validators.maxLength(200)]),
-    departmentId: new FormControl('', Validators.required),
-    accessLevel: new FormControl('', Validators.required),
+    departmentId: new FormControl(
+      this.departments[0].name,
+      Validators.required
+    ),
+    accessLevel: new FormControl(this.accessLevels[2], Validators.required),
     profileImgUrl: new FormControl<string>('', [Validators.required]),
   });
 
-  specialEditableOfficerForm = new FormGroup({
+  advancedOfficerForm = new FormGroup({
     firstName: new FormControl('', [
       Validators.minLength(1),
       Validators.maxLength(50),
@@ -71,4 +130,40 @@ export class OfficerIdPageComponent implements OnInit {
     ]),
     badgeNumber: new FormControl('', Validators.required),
   });
+
+  onBasicFormSubmit() {
+    throw new Error('Method not implemented.');
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      this.selectedImageUrl = URL.createObjectURL(file);
+      this.basicOfficerForm
+        .get('profileImgUrl')
+        ?.setValue(this.selectedImageUrl);
+    }
+  }
+
+  private _departmentFilter(value: string): Department[] {
+    const filterValue = value.toLowerCase();
+
+    const values = this.departments.filter((option) =>
+      option.name.toLowerCase().includes(filterValue)
+    );
+
+    if (values.length === 0) {
+      this.basicOfficerForm.get('departmentId')?.setErrors({
+        invalidOption: true,
+      });
+    }
+
+    return values;
+  }
+
+  ngOnDestroy() {
+    if (this.selectedImageUrl) {
+      URL.revokeObjectURL(this.selectedImageUrl);
+    }
+  }
 }
