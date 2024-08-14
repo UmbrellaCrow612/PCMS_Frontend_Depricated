35ee75675ee649cc75dd64eaacbeafc92393ed99
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-14 12:07:11 +0100

    profile page with dark mode toggle

--- PCMS.FrontEnd/src/app/settings/profile/profile-page.component.ts
+++ PCMS.FrontEnd/src/app/settings/profile/profile-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Subscription } from 'rxjs';
+import { ThemeService } from '../../theme/theme-service.service';
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule, MatSlideToggleModule],
   templateUrl: './profile-page.component.html',
-  styleUrl: './profile-page.component.scss'
+  styleUrl: './profile-page.component.scss',
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit, OnDestroy {
+  isDarkMode: boolean = false;
+  private subscription: Subscription | undefined;
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.subscription = this.themeService.isDarkMode$.subscribe(
+      (isDarkMode) => (this.isDarkMode = isDarkMode)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  toggleDarkMode() {
+    this.themeService.toggleDarkMode();
+  }
 }
