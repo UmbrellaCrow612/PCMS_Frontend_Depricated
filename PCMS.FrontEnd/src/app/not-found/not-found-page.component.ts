26b5fc5d3760c8e1556732f3b1d5651e0d5eb18b
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-15 16:16:43 +0100

    404 page link takes you back

--- PCMS.FrontEnd/src/app/not-found/not-found-page.component.ts
+++ PCMS.FrontEnd/src/app/not-found/not-found-page.component.ts
@@ -1,17 +1,22 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { Title } from '@angular/platform-browser';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [MatButtonModule],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
 })
 export class NotFoundPageComponent implements OnInit {
-  constructor(private title: Title) {}
+  constructor(private location: Location, private title: Title) {}
   ngOnInit(): void {
     this.title.setTitle('PCMS - Not Found');
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
