daf6d22c31b10419c045a107f7879c2afd952ff7
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-15 11:29:45 +0100

    basic quick actions cases homepage

--- PCMS.FrontEnd/src/app/cases/home/cases-home-page.component.ts
+++ PCMS.FrontEnd/src/app/cases/home/cases-home-page.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cases-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './cases-home-page.component.html',
   styleUrl: './cases-home-page.component.scss',
 })
-export class CasesHomePageComponent {}
+export class CasesHomePageComponent {
+  quickSearchForm = new FormGroup({
+    caseNumber: new FormControl('', [Validators.required]),
+  });
+
+  onQuickSearchFormSubmit() {
+    if (this.quickSearchForm.valid) {
+      console.log(this.quickSearchForm.value);
+    }
+  }
+}
