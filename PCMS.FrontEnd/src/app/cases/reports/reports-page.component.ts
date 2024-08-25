32dd660a23c56e3450ce4faa1f2414e8ebf8fa62
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-25 17:14:22 +0100

    reports page made

--- PCMS.FrontEnd/src/app/cases/reports/reports-page.component.ts
+++ PCMS.FrontEnd/src/app/cases/reports/reports-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { ColDef } from 'ag-grid-community';
+import { GridComponent } from '../../grid/grid.component';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-reports-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, GridComponent, RouterLink],
   templateUrl: './reports-page.component.html',
-  styleUrl: './reports-page.component.scss'
+  styleUrl: './reports-page.component.scss',
 })
 export class ReportsPageComponent {
+  columnDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+  ];
 
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 },
+  ];
 }
