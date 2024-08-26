4872c14854af32cee38f9ffe68d58333ddf8fffc
Yousaf Wazir <umbrellaman612@gmail.com>
2024-08-26 16:58:03 +0100

    fix error

--- PCMS.FrontEnd/src/app/features/cases/cases.service.ts
+++ PCMS.FrontEnd/src/app/features/cases/cases.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
 import { Case } from './interfaces';
+import { environment } from '../../../environments/environment';
 
 
 @Injectable({
